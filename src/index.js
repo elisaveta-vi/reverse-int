42ed055d02e72e05eadb7ec053e7a8ef3a1b8c75
Kalinkina Elizaveta <kaliveta@gmail.com>
2022-08-10 20:08:55 +0800

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+
+	let number = Math.abs(n);
+	number = number + '';
+	return number.split('').reverse().join('');
+
 }
+
